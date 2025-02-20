81230ce3f28d8994f5c0bfa74fccdd5a1b958ac9
MarcoADG <marcoaureliodgomes@gmail.com>
2025-02-20 16:17:25 -0300

    feature: adicionando 0auth

---
+++ src/features/auth/components/types.ts
@@ -0,0 +1,9 @@
+import { SignInFlow } from "../types";
+
+export interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
